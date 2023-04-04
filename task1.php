928160039fed298ef8ada92881288ba5321869b3
ehabakl <118850488+ehabakl@users.noreply.github.com>
2023-04-04 10:41:01 +0300

    Update task1.php

--- task1.php
+++ task1.php
@@ -5,28 +5,16 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <style>
-      span{ 
-    color : red;
-    font-size:large;
-}
-table{ 
-    font-size: large;
-}
-h1{ text-align: center;}
-    </style>
-
-    
 </head>
-<body >
+<body>
 <?php
 $nameErr = $emailErr = $genderErr = $subErr = "";
 $name = $email = $gender = $submit  = "";
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  if (empty($_POST["fname"])) {
+  if (empty($_POST["name"])) {
     $nameErr = "Name is required";
   } else {
-    $name = test_input($_POST["fname"]);
+    $name = test_input($_POST["name"]);
   }
   
   if (empty($_POST["email"])) {
@@ -35,10 +23,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = test_input($_POST["email"]);
   }
     
-  if (empty($_POST["submit"])) {
+  if (empty($_POST["agree"])) {
     $subErr= "You must agree to terms";
   } else {
-    $submit = test_input($_POST["submit"]);
+    $submit = test_input($_POST["agree"]);
   }
 
   if (empty($_POST["gender"])) {
@@ -49,50 +37,73 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 }
 ?>
 
-
-<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"  method="POST"  >
-    <table align="center" >
-        <thead ><h1>Applications name : AAST_BIS class registration</h1></thead>
-        <tbody>
-            <th ><span>* Required field </span></th>
-            <tr><td>Name:</td><td><input type="text" name="fname"><span>*<?php echo $nameErr;?></span></span></td></tr>
-            <tr><td>Email:</td><td><input type="email" name="email"><span>*<?php echo $emailErr;?></span></span></td></tr>
-            <tr><td>Group#</td><td><input type="text" name="group"></td></tr>
-            <tr><td>Class details</td><td><textarea name="textarea" cols="50" rows="5"></textarea></td></tr>
-            <tr><td>Gender</td><td><label for="male">Male</label><input value="Male" name="gender" type="radio">
-                <label for="female">Female</label><input value="Female" name="gender" type="radio"><span>*<?php echo $genderErr;?></span></span></td>
-            </tr>
-            <tr><td>Select Courses :</td>
-                <td><select multiple="multiple" name="multiselec[]">
-                    <option value="PHP">PHP</option>
-                    <option value="CSS">CSS</option>
-                    <option value="HTML">HTML</option>
-                    <option value="JavaScript">JavaScript</option>
-                    <option value="MySQL">MySQL</option>
-                </select></td></tr>
-        <tfoot><td>Agree<br><input type="submit" name="submit"></td><td><input type="checkbox"><span>*<?php echo $subErr;?></span></span></td></tfoot>
-        </tbody>
-    </table>
-</form> 
-
-<?php
-
-function test_input($data) {
-  $data = trim($data);
-  $data = stripslashes($data);
-  $data = htmlspecialchars($data);
-  return $data;}
-  echo "Name : ".$_POST["fname"]."<br>" ;
-  echo "E-mail : ".$_POST["email"]."<br>" ;
-  echo "Group # : ".$_POST["group"]."<br>" ;
-  echo "Class details : ".$_POST["textarea"]."<br>" ;
-  echo "Gender : ".$_POST["gender"]."<br>" ;
-  if(isset($_POST['multiselec'])) {
-    echo "Your Courses :" ;
-    foreach ($_POST['multiselec'] as $selectedOption) {
-        echo $selectedOption." ";
-    }
-  }
-?>
+<form action="<?php $_PHP_SELF ?>" method="post" >   
+            <table align="center" >
+                
+                    <tr>
+                        <td><p><span style="color:red">*</span> required field</p></th>
+                    </tr>
+                    <tr>
+                        <td><label for="">name</label></th>
+                        <td><input type="text" name="name"><span style="color:red">*<?php echo $nameErr ?></span></th>
+                        
+                    </tr>
+                    <tr>
+                        <td><label for="">email</label></td>
+                        <td><input type="email" name="email"><span style="color:red">*<?php echo $emailErr ?></span></td>
+                    </tr>
+                    <tr>
+                        <td><label for="">Group #</label></td>
+                        <td><input type="text" name ="group"></td>
+                    </tr>
+                    <tr>
+                        <td><label for="">class details :</label></td>
+                        <td><textarea name="class_details" id="" cols="30" rows="5"></textarea></td>
+                    </tr>
+                    <tr>
+                        <td><label for="">gender</label></td>
+                        <td><input type="radio" name="gender" value="female">female <input type="radio" name="gender" value = "male">male <span style="color:red">*<?php echo $genderErr ?></span></td>
+                    </tr>
+                    <tr>
+                        <td><label for="">select courses</label></td>
+                        <td> 
+                            <select size="4" multiple name="courses">
+                                <option value='php'>php</option>
+                                <option value='javascript'>javascript</option>
+                                <option value='mysql'>mysql</option>
+                                <option value='html'>html</option>
+                            </select>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td><label for="">agree</label></td>
+                        <td><input type="checkbox" name ="agree"> <span style="color:red">*</span></td>
+                    </tr>
+                    <tr>
+                        <td><input type="submit" name ="submit"></td>
+                    </tr>
+            </table>
+        </form> 
 </body>
 </html>
+
+           
+<?php
+    function test_input($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+        }
+
+
+    echo "Name:".$_POST['name']."<br>";
+    echo "Email:".$_POST['email']."<br>";
+    echo "Group #:".$_POST['group']."<br>";
+    echo "Class details:".$_POST['class_details']."<br>";
+    echo "gender:".$_POST['gender']."<br>";
+    echo "your courses are:".$_POST['courses']."<br>";
+
+
+
+?>
